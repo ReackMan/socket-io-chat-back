680814972aab333d25b13e2a176f63313f383e86
ReackMan <reackman@yandex.ru>
2025-08-12 20:27:32 +0300

    add new message

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,30 @@ const io = new Server(server, {
   },
 })
 
+const messages = [
+  { message: 'Ola Kate', id: 123321, user: { id: 123, name: 'Anna' } },
+  { message: 'Ola Anna', id: 123322, user: { id: 321, name: 'Kate' } },
+]
+
 app.get('/', (req, res) => {
   res.send(`Hello, It's server`)
 })
 
 io.on('connection', socket => {
   console.log('a user connected')
+
+  socket
+    .on('client-message-was-sent', (message: string) => {
+      console.log(message)
+      let messageItem = {
+        message: message,
+        id: 123321 + new Date().getTime(),
+        user: { id: 123, name: 'Anna' },
+      }
+      messages.push(messageItem)
+      io.emit('new-message-sent', messageItem)
+    })
+    .emit('init-messages-was-published', messages)
 })
 
 const PORT = process.env.PORT || 3009
